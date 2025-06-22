3ad8f1d695ee2d9d0d1299c8a1503f42bf9ff6d0
Kaa <31796925+ItsKaa@users.noreply.github.com>
2025-06-22 23:07:09 +0200

    Hook HUD visibility. Use a queue to update the visiblity.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "screen_percentage.h"
 #include <string>
 #include <fstream>
+#include <queue>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <safetyhook.hpp>
@@ -13,6 +14,14 @@ HMODULE this_module;
 HMODULE exe_module = GetModuleHandle(NULL);
 std::filesystem::path module_path;
 std::uint8_t* screen_percentage_address = nullptr;
+
+std::queue<int> queue_screen_percentage_updates;
+std::mutex queue_mutex;
+int screen_percentage_update_delay = 2000;
+int screen_percentage_default = 100;
+int screen_percentage_photos = 200;
+bool is_game_paused = false;
+bool restore_screen_percentage_on_unfreeze = false;
 
 void LogAllValues(const char* title, const safetyhook::Context& ctx)
 {
@@ -57,6 +66,7 @@ void HookGameFreeze()
                 && ctx.r12 == 1 && ctx.r15 == 1)
             {
                 spdlog::info("Detected game freeze.");
+                is_game_paused = true;
             }
         });
     } else
@@ -79,12 +89,55 @@ void HookGameUnfreeze()
                 && ctx.rcx == 0 && ctx.rdi == 0 && ctx.r13 == 0)
             {
                 spdlog::info("Detected game unfreeze.");
+                is_game_paused = false;
+                if (restore_screen_percentage_on_unfreeze)
+                {
+                    queue_screen_percentage_updates.push(screen_percentage_default);
+                }
             }
         });
     }
     else
     {
         spdlog::error("Failed to find the address for game unfreeze!");
+    }
+}
+
+void HookPhotoModeHUDVisibility()
+{
+    if (std::uint8_t* result = PatternScan(exe_module, "0F 94 ?? 33 ?? 88 81 ?? ?? ?? ?? 48 ?? ?? C7"))
+    {
+        spdlog::info("Found address for PhotoMode HUD Visibility: 0x{:X}", reinterpret_cast<uintptr_t>(result));
+        static SafetyHookMid hook = safetyhook::create_mid(result + 0x05, [](safetyhook::Context& ctx) {
+            if ((ctx.rdx == 1 || ctx.rdx == 0) && ctx.rbx == 0 )
+            {
+                const auto hud_visible = ctx.rdx == 1;
+                spdlog::debug("Found HUD toggle {:s}", hud_visible ? "true" : "false");
+                LogAllValues("HUD Visibility", ctx);
+
+                if (!is_game_paused)
+                {
+                    spdlog::warn("Detected HUD toggle but game is not paused. Aborting.");
+                    return;
+                }
+
+                std::unique_lock lock(queue_mutex);
+                if (hud_visible)
+                {
+                    spdlog::info("HUD visible, resetting screen percentage value to {:d}", screen_percentage_default);
+                    queue_screen_percentage_updates.push(screen_percentage_default);
+                }
+                else
+                {
+                    spdlog::info("HUD hidden, updating screen percentage value to {:d}", screen_percentage_photos);
+                    queue_screen_percentage_updates.push(screen_percentage_photos);
+                }
+            }
+        });
+    }
+    else
+    {
+        spdlog::error("Failed to find address for HUD Visibility!");
     }
 }
 
@@ -114,6 +167,7 @@ DWORD WINAPI Main(void*)
     HookTemporalAASamples();
     HookGameFreeze();
     HookGameUnfreeze();
+    HookPhotoModeHUDVisibility();
 
     if (screen_percentage_address = FindScreenPercentageAddress();
         screen_percentage_address == nullptr)
@@ -129,6 +183,28 @@ DWORD WINAPI Main(void*)
     return 1;
 }
 
+DWORD WINAPI HandleScreenPercentageUpdates(void*)
+{
+    using namespace std::literals::chrono_literals;
+    while (true)
+    {
+        std::unique_lock lock(queue_mutex);
+        int value = 0;
+        if (!queue_screen_percentage_updates.empty())
+        {
+            value = queue_screen_percentage_updates.front();
+            queue_screen_percentage_updates.pop();
+        }
+        if (value > 0)
+        {
+            WriteScreenPercentageFixLag(screen_percentage_address, value, value <= screen_percentage_default ? screen_percentage_update_delay : 0);
+        }
+
+        std::this_thread::sleep_for(100ms);
+    }
+
+    return 1;
+}
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID)
 {
@@ -141,6 +217,10 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID)
             {
                 SetThreadPriority(main_thread, THREAD_PRIORITY_HIGHEST);
                 CloseHandle(main_thread);
+            }
+            if (auto thread = CreateThread(nullptr, 0, HandleScreenPercentageUpdates, nullptr, 0, nullptr))
+            {
+                CloseHandle(thread);
             }
             break;
         }
