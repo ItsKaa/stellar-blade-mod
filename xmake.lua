fb9c2af7356437bca9bc56dbf65ec6801d370fc0
Kaa <31796925+ItsKaa@users.noreply.github.com>
2025-06-28 21:39:25 +0200

    Update xmake config.

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,30 @@
 add_rules("mode.debug", "mode.release")
---set_warnings("all", "error")
-set_languages("clatest", "cxxlatest")
+set_languages("c17", "cxxlatest")
 set_encodings("utf-8")
-set_optimize("smallest")
+
+if is_plat("windows") then
+    set_toolchains("msvc")
+    add_cxflags("/GL")
+    add_ldflags("/LTCG", "/OPT:REF", "/OPT:ICF")
+    set_runtimes("MT")
+end
+
+if is_mode("debug") then
+    set_symbols("hidden")
+    set_optimize("none")
+else
+    set_optimize("smallest")
+end
 
 add_requires("vcpkg::spdlog",     {alias = "spdlog"})
 add_requires("vcpkg::safetyhook", {alias = "safetyhook"})
-add_requires("vcpkg::inipp",      {alias = "inipp", configs = { features = {"wchar"} }})
+add_requires("vcpkg::inipp",      {alias = "inipp"})
 add_requires("vcpkg::fmt",        {alias = "fmt"})
 add_requires("vcpkg::zydis",      {alias = "zydis"})
 
 target("PhotoModePatches")
     set_kind("shared")
+    set_extension(".asi")
     set_prefixname("")
     add_files("src/*.cpp")
     add_packages("safetyhook")
@@ -20,9 +33,3 @@ target("PhotoModePatches")
     add_packages("fmt")
     add_packages("zydis")
     add_syslinks("user32")
-
-
-if is_plat("windows") then
-    set_extension(".asi")
-    set_toolchains("msvc")
-end
