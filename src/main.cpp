1c674b8943a7a7ffb124e30d5f950383c646be72
Kaa <31796925+ItsKaa@users.noreply.github.com>
2025-06-28 20:44:28 +0200

    Delete log every session when in debug.

--- src/main.cpp
+++ src/main.cpp
@@ -173,6 +173,16 @@ void InitLogging()
     module_path = std::filesystem::path(buffer);
     const auto game_path = std::filesystem::path(module_path).remove_filename();
 
+    // Remove the existing log file when debugging.
+    // Handy when testing patterns.
+#   ifdef _DEBUG
+    auto log_path = game_path / (dll_name + ".log");
+    if (std::filesystem::exists(log_path))
+    {
+        std::filesystem::remove(log_path);
+    }
+#   endif
+
     spdlog::set_default_logger(std::make_shared<spdlog::logger>("",
                                                                 std::make_shared<spdlog::sinks::rotating_file_sink_st>(
                                                                     game_path.native() + std::wstring(dll_name.begin(), dll_name.end()) + L".log",
